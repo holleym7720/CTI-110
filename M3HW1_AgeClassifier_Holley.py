44a6777ef992966d45487b20e096d96f57a96d0a
holleym7720 <31663165+holleym7720@users.noreply.github.com>
2017-09-24 19:14:52 -0400

    M3HW1_AgeClassifier_holley.py

---
+++ M3HW1_AgeClassifier_Holley.py
@@ -0,0 +1,19 @@
+#CTI-110
+#M3HW1 - Age Classifier
+#Marcus Holley
+
+#Identify your age
+age= int(input('Enter Your Age: '))
+
+if age <= 1:
+    print('Infant')
+elif age < 13:
+    print('Child')
+elif age <20:
+    print('Teenager')
+elif age>=20:
+    print ('Adult')
+
+
+
+    
