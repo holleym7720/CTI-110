37a37a3a7ae1107b73e54568e854a7ff2bf0aa64
holleym7720 <31663165+holleym7720@users.noreply.github.com>
2017-09-24 21:14:45 -0400

    M3Q2A-M_holley.py

---
+++ M3Q2A-M_Holley.py
@@ -0,0 +1,25 @@
+#M3Q2A-M
+#Marcus Holley
+#24Sept17
+
+def main():
+#this program shows the state of water during temperature change
+
+#system uses a 3 stage scale
+    Ice = 0
+    LiquidWater = 100
+    Steam = 100
+temperature = input( 'Whats the Temperature' )
+
+if temperature <0:
+ print( 'Ice' )
+elif temperture <100:
+ print('Liquid Water')
+else:
+ print('Steam' )
+
+temperature=int(raw_input("enter a temperature in Celsius:"))
+fahrenheit=9.0/5.0 * celsius + 32
+print("temperature:", Celsius, "Celcius=",Fahrenheit,"F")
+
+main()
